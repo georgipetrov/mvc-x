d1bb3338d1322d1e7c739d49f867a0ddb1bfbab6
Georgi Petrov <conicgp@gmail.com>
2015-03-02 01:25:50 +0200

    Assets support added

---
+++ .mvcx/asset.php
@@ -0,0 +1,30 @@
+<?php
+$config = array();
+foreach(glob('../app/*', GLOB_ONLYDIR) as $dir) {
+    if (file_exists($dir.'/config.php')) {
+		include $dir.'/config.php';
+	}
+}
+include('app.class.php');
+$app = new App;
+$app_info =$app->getAppByUrl($config);
+$asset = '../app/'.$app_info['dir'].'/view/asset/'.$_GET['file'];
+
+if (!file_exists($asset)){
+	header('HTTP/1.0 404 Not Found');
+	echo 'RESOURCE NOT FOUND';exit;
+}
+
+$contentType = 'text/plain';
+
+try {
+	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+	$contentType = finfo_file($finfo, $asset);
+	finfo_close($finfo);
+} catch (Exception $e) {
+	
+}
+
+header('content-type:'.$contentType);
+readfile($asset);
+?>
