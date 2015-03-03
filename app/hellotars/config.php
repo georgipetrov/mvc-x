b7aeed3806eb99b35417fcf16265db8279060d98
Georgi Petrov <conicgp@gmail.com>
2015-03-03 14:03:16 +0200

    Config improvement

--- app/hellotars/config.php
+++ app/hellotars/config.php
@@ -2,12 +2,12 @@
 /*** CONFIGURE THIS APP BELOW ***/
 
 /*	 DOMAIN, DIRECTORY AND DATABASE
- * 	 url - Here you should add your site public url without protocol and subdomain, e.g. mysite.com 
- *   dir - The name of the app directory on your server e.g. mysite
+ * 	 url - Here you should add your site public url without protocol and without trailing slash, e.g. mysite.com 
+ *   dir - The name of the app directory in /app/ folder. e.g. mysite
  *   db - The database configuration of the app
- *   template - Specifies the name of the template you want to use. The template name should be a folder in /view/. If set to false, it will not use a template.
- *   smart_elements - If true, the response will parse all smart elements. A smart element is e.g. [widget:breadcrumb]
- *   debug_mode - If you enable debug mode, you will see debug information at the bottom of your page. To put this app in debug mode you need to set it to 1, otherwise leave it 0.
+ *   template - Accepts boolean or string. Specifies the name of the template you want to use. The template name should be a folder in /view/. If set to false, it will not use a template.
+ *   smart_elements - Accepts boolean. If true, the response will parse all smart elements. A smart element is e.g. [widget:breadcrumb]
+ *   debug_mode - Accepts integer 0 and 1. If set to 1, you will see debug information at the bottom of your page. 
  */
 
 $config[] = array(
