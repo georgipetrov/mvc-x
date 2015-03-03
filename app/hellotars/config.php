e31a3608aeb3b1cf18cd5ebb5a704387fa87ef03
Georgi Petrov <conicgp@gmail.com>
2015-03-03 15:08:37 +0200

    Config fixes

--- app/hellotars/config.php
+++ app/hellotars/config.php
@@ -5,7 +5,7 @@
  * 	 url - Here you should add your site public url without protocol and without trailing slash, e.g. mysite.com 
  *   dir - The name of the app directory in /app/ folder. e.g. mysite
  *   db - The database configuration of the app
- *   template - Accepts boolean or string. Specifies the name of the template you want to use. The template name should be a folder in /view/. If set to false, it will not use a template.
+ *   template - Accepts boolean or string. Specifies the name of the template you want to use. The template name should be a folder in /view/template/. If set to false, it will not use a template.
  *   smart_elements - Accepts boolean. If true, the response will parse all smart elements. A smart element is e.g. [widget:breadcrumb]
  *   debug_mode - Accepts integer 0 and 1. If set to 1, you will see debug information at the bottom of your page. 
  */
@@ -21,7 +21,7 @@ $config[] = array(
 	'password'=>'',
 	'table_prefix'=>''
 	),
-'template'=> 'default',
+'template'=> false,
 'smart_elements'=> true,
 'debug_mode'=> 1
 );
