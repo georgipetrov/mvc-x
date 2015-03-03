059ed567f83ab4e66dfdac5935051dc3eb9f6b63
Georgi Petrov <conicgp@gmail.com>
2015-03-03 13:41:46 +0200

    Improved config

--- app/hellotars/config.php
+++ app/hellotars/config.php
@@ -2,9 +2,11 @@
 /*** CONFIGURE THIS APP BELOW ***/
 
 /*	 DOMAIN, DIRECTORY AND DATABASE
- * 	 url - here you should add your site public url without protocol and subdomain, e.g. mysite.com 
- *   dir - the name of the app directory on your server e.g. mysite
- *   db - the database configuration of the app
+ * 	 url - Here you should add your site public url without protocol and subdomain, e.g. mysite.com 
+ *   dir - The name of the app directory on your server e.g. mysite
+ *   db - The database configuration of the app
+ *   template - Specifies the name of the template you want to use. The template name should be a folder in /view/. If set to false, it will not use a template.
+ *   smart_tags - If true, the response will parse all smart tags. A smart tag is e.g. [widget:breadcrumb]
  *   debug_mode - If you enable debug mode, you will see debug information at the bottom of your page. To put this app in debug mode you need to set it to 1, otherwise leave it 0.
  */
 
@@ -19,6 +21,7 @@ $config[] = array(
 	'password'=>'',
 	'table_prefix'=>''
 	),
+'template'=> false,
 'smart_tags'=> true,
 'debug_mode'=> 0
 );
